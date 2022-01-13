80a86b23c973ea9288bee193fc98dd78b5ff7b97
Asel Zhumalieva <asjumalieva@gmail.com>
2022-01-13 15:53:16 +0300

    Initial commit Pushing project

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException{
+        while(true){
+            Thread.sleep(500);
+
+            System.out.println("Tik");
+
+            Thread.sleep(500);
+
+            System.out.println("Tak");
+
+        }
+    }
+}
+
